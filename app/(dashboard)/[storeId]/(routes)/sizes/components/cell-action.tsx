d8f6d9666e5d5f4a16199e798c46262eb79ab0ae
summer030 <125527872+summer030@users.noreply.github.com>
2023-08-30 22:20:02 +0800

    feat: sizes entity

---
+++ app/(dashboard)/[storeId]/(routes)/sizes/components/cell-action.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import axios from "axios";
+import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useParams, useRouter } from "next/navigation";
+import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import AlertModal from "@/components/modals/alert-modal";
+
+import { SizeColumn } from "./columns";
+
+interface CellActionProps {
+  data: SizeColumn;
+}
+
+export const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const router = useRouter();
+  const params = useParams();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onConfirm = async () => {
+    try {
+      setLoading(true);
+      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      router.refresh();
+      router.push(`/${params.storeId}/sizes`);
+      toast.success("Size deleted.");
+    } catch (error) {
+      toast.error("Make sure you removed all products using this size");
+    } finally {
+      setLoading(false);
+      setOpen(false);
+    }
+  };
+
+  const onCopy = (id: string) => {
+    navigator.clipboard.writeText(id);
+    toast.success("Size Id copied to clipboard.");
+  };
+  return (
+    <>
+      <AlertModal
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onConfirm}
+        loading={loading}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" className="h-8 w-8 p-0">
+            <span className="sr-only">Open menu</span>
+            <MoreHorizontal className="h-4 w-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>Actions</DropdownMenuLabel>
+          <DropdownMenuItem onClick={() => onCopy(data.id)}>
+            <Copy className="h-4 w-4 mr-2" /> Copy Id
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
+          >
+            <Edit className="h-4 w-4 mr-2" /> Update
+          </DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setOpen(true)}>
+            <Trash className="h-4 w-4 mr-2" /> Delete
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+};
