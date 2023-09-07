58723847f3f4784e1e2c18182749b5f5ef561352
summer030 <125527872+summer030@users.noreply.github.com>
2023-09-07 22:30:57 +0800

    fix: router errors

--- app/api/[storeId]/categories/[categoryId]/route.ts
+++ app/api/[storeId]/categories/[categoryId]/route.ts
@@ -20,6 +20,9 @@ export async function GET(
       where: {
         id: params.categoryId,
       },
+      include: {
+        billboard: true,
+      },
     });
 
     return NextResponse.json(category);
