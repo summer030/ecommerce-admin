ca1e24020c1bc96f4923cd6b841437970d185e7e
summer030 <karaage4projects@outlook.com>
2023-09-19 21:09:21 +0800

    feat: dark mode

--- components/narbar.tsx
+++ components/narbar.tsx
@@ -1,8 +1,10 @@
 import { UserButton, auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 import { MainNav } from "@/components/main-nav";
 import StoreSwitcher from "@/components/store-switcher";
-import { redirect } from "next/navigation";
 import prismadb from "@/lib/prismadb";
+import { ThemeToggle } from "@/components/theme-toggle";
 
 const Narbar = async () => {
   const { userId } = auth();
@@ -22,6 +24,7 @@ const Narbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
           <UserButton afterSignOutUrl="/" />
         </div>
       </div>
